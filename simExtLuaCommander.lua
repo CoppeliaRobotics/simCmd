57c2bec5bc3e226529a352fb52a75637aebded1c
Federico Ferri <federico.ferri.it@gmail.com>
2018-05-08 13:35:14 +0200

    document all keyboard shortcuts

--- simExtLuaCommander.lua
+++ simExtLuaCommander.lua
@@ -2,16 +2,24 @@ local simLuaCmd={}
 
 html_help = [[
         <h2>LuaCommander plugin</h2>
-        Use this plugin for quick evaluation of Lua expressions.
-
-        <h3>Keyboard Shortcuts</h3>
-        Press Ctrl+Alt+C to focus the text input.
+        <p>Use this plugin for quick evaluation of Lua expressions.</p>
 
         <h3>Completion</h3>
-        Begin to type the name of a function (e.g. "sim.getObjectHa") and press TAB to automatically complete it. If there are multiple matches, repeatedly press TAB to cycle through completions, and Shift+TAB to cycle back.
+        <p>Begin to type the name of a function (e.g. "sim.getObjectHa") and press TAB to automatically complete it. If there are multiple matches, repeatedly press TAB to cycle through completions, and Shift+TAB to cycle back.</p>
+
+        <h3>Keyboard Shortcuts</h3>
+        <p>
+        <b>Ctrl+Alt+C</b>: focus the text input.<br/>
+        <b>TAB</b>: complete symbol / cycle to next completion.<br/>
+        <b>Shift+TAB</b>: cycle to previous completion.<br/>
+        <b>Enter</b>: accept completion (also works with '(' and '.').<br/>
+        <b>Esc</b>: clear text field.<br/>
+        <b>Up/Down</b> arrows: navigate through command history.<br/>
+        <b>Ctrl+L</b>: clear statusbar.
+        </p>
 
         <h3>String Rendering Flags</h3>
-        There are some flags that control how the results are displayed. Those are input by adding a comment at the end of the line, containing as comma separated list of key=value pairs, e.g.: "luaExpression --flag1=10,flag2=test". Flags can be abbreviated by typing only the initial part, e.g. "pre" instead of "precision", down to any length, provided it is not ambiguous.
+        <p>There are some flags that control how the results are displayed. Those are input by adding a comment at the end of the line, containing as comma separated list of key=value pairs, e.g.: "luaExpression --flag1=10,flag2=test". Flags can be abbreviated by typing only the initial part, e.g. "pre" instead of "precision", down to any length, provided it is not ambiguous.</p>
         <ul>
             <li><b>depth</b>: (integer) limit the maximum depth when rendering a map-table.</li>
             <li><b>precision</b>: (integer) number of floating point digits.</li>
@@ -24,17 +32,13 @@ html_help = [[
         </ul>
 ]]
 
-function helpclose()
-    if ui then
+function help()
+    if ui then  
         simUI.destroy(ui)
         ui = nil
+    else
+        ui = simUI.create('<ui title="LuaCommander Plugin" closeable="true" on-close="help" modal="true" size="440,520"><text-browser text="'..string.gsub(html_help,'"','&quot;')..'" /></ui>')
     end
-end
-
-function help()
-    ui = simUI.create([[<ui title="LuaCommander Plugin" closeable="true" on-close="helpclose" modal="true" size="400,500">
-        <text-browser text="]]..string.gsub(html_help,'"','&quot;')..[[" />
-    </ui>]])
 end
 
 return simLuaCmd
