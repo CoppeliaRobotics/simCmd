9d35e86466010332fc44bf74cf6ec390e9680123
Federico Ferri <federico.ferri.it@gmail.com>
2018-06-10 14:29:11 +0200

    add module info

--- plugin.cpp
+++ plugin.cpp
@@ -149,6 +149,9 @@ public:
         if(!registerScriptStuff())
             throw std::runtime_error("failed to register script stuff");
 
+        simSetModuleInfo(PLUGIN_NAME, 0, "Lua REPL (read-eval-print-loop) Plugin", 0);
+        simSetModuleInfo(PLUGIN_NAME, 1, __DATE__, 0);
+
         optionsChangedFromGui.store(false);
         optionsChangedFromData.store(false);
 
