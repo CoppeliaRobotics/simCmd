63f29e1359f24dc70a67ceba7c1fb02d5409ee68
Federico Ferri <federico.ferri.it@gmail.com>
2022-04-14 11:06:26 +0200

    make other funcs local

--- test/getCallContext.lua
+++ test/getCallContext.lua
@@ -91,7 +91,7 @@ local function findCallsAtPosition(ast,pos,results)
     return results
 end
 
-function dump(o,indent)
+local function dump(o,indent)
     indent=indent or ''
     if type(o)=='table' then
         local s='{\n'
