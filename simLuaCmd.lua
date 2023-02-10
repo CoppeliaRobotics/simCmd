d2c7d818f50e7f8b8c5d88987b8c7497a2fae966
Federico Ferri <federico.ferri.it@gmail.com>
2023-02-10 15:57:03 +0100

    document some new features

--- simLuaCmd.lua
+++ simLuaCmd.lua
@@ -21,8 +21,8 @@ html_help = [[
         <h3>String Rendering Flags</h3>
         <p>There are some flags that control how the results are displayed. Those are input by adding a comment at the end of the line, containing as comma separated list of key=value pairs, e.g.: "luaExpression --flag1=10,flag2=test". Flags can be abbreviated by typing only the initial part, e.g. "pre" instead of "precision", down to any length, provided it is not ambiguous.</p>
         <ul>
-            <li><b>depth</b>: (integer) limit the maximum depth when rendering a map-table.</li>
-            <li><b>precision</b>: (integer) number of floating point digits.</li>
+            <li><b>depth</b>: (int) limit the maximum depth when rendering a map-table.</li>
+            <li><b>precision</b>: (int) number of floating point digits.</li>
             <li><b>retvals</b>: (1, *) print all the returned values (*) or only the first (1).</li>
             <li><b>sort</b>: (k, t, tk, off) how to sort map-table entries:
                     k: sort by keys;
@@ -32,10 +32,24 @@ html_help = [[
             </li>
             <li><b>escape</b>: (0/1) enable/disable special character escaping.</li>
         </ul>
+
+        <h3>Special variables</h3>
+        <p>Some special variables are set automatically before each evaluation:</p>
+        <ul>
+            <li><b>H</b>: (function) alias for sim.getObject, e.g. H'/foo' as a shortcut for sim.getObject('/foo'); it also accepts TAB-completion.</li>
+            <li><b>SEL</b>: (table) the current object selection, i.e. a list of handles.</li>
+            <li><b>SEL1</b>: (int) the first selected object, i.e. SEL[1].</li>
+        </ul>
+
+        <h3>Misc. utility functions</h3>
+        <ul>
+            <li><b>apropos</b>: use this to search in the API symbols (functions and constants), e.g. apropos'inertia'.</li>
+            <li><b>printBytes</b>: print a binary string in hexadecimal bytes, like hexdump.</li>
+        </ul>
 ]]
 
 function help()
-    if ui then  
+    if ui then
         simUI.destroy(ui)
         ui = nil
     else
