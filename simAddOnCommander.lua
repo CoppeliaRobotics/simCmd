9a50d50e02f0f8762bedd85f2afec13c52ca1e00
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-10 15:35:00 +0200

    skip lua formatter block

--- simAddOnCommander.lua
+++ simAddOnCommander.lua
@@ -2,21 +2,22 @@ sim=require'sim'
 
 options={}
 
-menus={
+-- LuaFormatter off
+menus = {
     {
-        label='',
-        enabled=true,
+        label = '',
+        enabled = true,
     },
     {
-        label='Clear command history',
-        enabled=true,
-        callback=function(self)
+        label = 'Clear command history',
+        enabled = true,
+        callback = function(self)
             simCmd.clearHistory()
         end,
     },
     {
-        label='',
-        enabled=true,
+        label = '',
+        enabled = true,
     },
     --[[
     -- non-boolean options:
@@ -28,133 +29,134 @@ menus={
     --]]
     --[[
     {
-        label='Print all returned values',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.printAllReturnedValues',self.checked)
+        label = 'Print all returned values',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.printAllReturnedValues', self.checked)
         end,
     },
     {
-        label='Warn about multiple returned values',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.warnAboutMultipleReturnedValues',self.checked)
+        label = 'Warn about multiple returned values',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.warnAboutMultipleReturnedValues', self.checked)
         end,
     },
     {
-        label='String rendering: escape special characters',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.stringEscapeSpecials',self.checked)
+        label = 'String rendering: escape special characters',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.stringEscapeSpecials', self.checked)
         end,
     },
     {
-        label='Map/array rendering: sort keys by name',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.mapSortKeysByName',self.checked)
+        label = 'Map/array rendering: sort keys by name',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.mapSortKeysByName', self.checked)
         end,
     },
     {
-        label='Map/array rendering: sort keys by type',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.mapSortKeysByType',self.checked)
+        label = 'Map/array rendering: sort keys by type',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.mapSortKeysByType', self.checked)
         end,
     },
     {
-        label='Map/array rendering: shadow long strings',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.mapShadowLongStrings',self.checked)
+        label = 'Map/array rendering: shadow long strings',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.mapShadowLongStrings', self.checked)
         end,
     },
     {
-        label='Map/array rendering: shadow buffer strings',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.mapShadowBufferStrings',self.checked)
+        label = 'Map/array rendering: shadow buffer strings',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.mapShadowBufferStrings', self.checked)
         end,
     },
     {
-        label='Map/array rendering: shadow strings with special characters',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.mapShadowSpecialStrings',self.checked)
+        label = 'Map/array rendering: shadow strings with special characters',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.mapShadowSpecialStrings', self.checked)
         end,
     },
     ]]--
     {
-        label='History: skip repeated commands',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.historySkipRepeated',self.checked)
+        label = 'History: skip repeated commands',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.historySkipRepeated', self.checked)
         end,
     },
     {
-        label='History: remove duplicates',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.historyRemoveDups',self.checked)
+        label = 'History: remove duplicates',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.historyRemoveDups', self.checked)
         end,
     },
     {
-        label='History: show matching entries (select with UP)',
-        enabled=true,
-        checkable=true,
-        checked=false,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.showMatchingHistory',self.checked)
+        label = 'History: show matching entries (select with UP)',
+        enabled = true,
+        checkable = true,
+        checked = false,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.showMatchingHistory', self.checked)
         end,
     },
     {
-        label='Set convenience vars',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.setConvenienceVars',self.checked)
+        label = 'Set convenience vars',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.setConvenienceVars', self.checked)
         end,
     },
     {
-        label='Auto-accept common completion prefix',
-        enabled=true,
-        checkable=true,
-        checked=true,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.autoAcceptCommonCompletionPrefix',self.checked)
+        label = 'Auto-accept common completion prefix',
+        enabled = true,
+        checkable = true,
+        checked = true,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.autoAcceptCommonCompletionPrefix', self.checked)
         end,
     },
     {
-        label='Resize statusbar when focused',
-        enabled=true,
-        checkable=true,
-        checked=false,
-        callback=function(self)
-            sim.setNamedBoolParam('simCmd.resizeStatusbarWhenFocused',self.checked)
+        label = 'Resize statusbar when focused',
+        enabled = true,
+        checkable = true,
+        checked = false,
+        callback = function(self)
+            sim.setNamedBoolParam('simCmd.resizeStatusbarWhenFocused', self.checked)
         end,
     },
 }
+-- LuaFormatter on
 
 function menuItemState(menu)
     local state=0
